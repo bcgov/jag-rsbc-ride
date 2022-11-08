6976775d1403ca6ae302a01bbda874cbe59cf39e
Amlan Chakladar <57117462+amlanc1@users.noreply.github.com>
2022-11-08 14:17:04 -0800

    wip

---
+++ rsbc-ride-producer-api/src/main/java/bcgov/rsbc/ride/kafka/dfProducer.java
@@ -0,0 +1,36 @@
+package bcgov.rsbc.ride.kafka;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import java.time.Duration;
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
+
+import javax.inject.Inject;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.POST;
+
+import javax.ws.rs.core.Response;
+import org.eclipse.microprofile.reactive.messaging.Channel;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import io.smallrye.reactive.messaging.MutinyEmitter;
+import io.smallrye.reactive.messaging.kafka.Record;
+
+import bcgov.rsbc.ride.kafka.model.testevent;
+
+@Path("/dfevents")
+public class dfProducer {
+
+
+
+    @GET
+    @Path("/ping")
+    @Produces(MediaType.TEXT_PLAIN)
+    public String dfping() {
+        return "df";
+    }
+}
