e5f04718e519562ab5cabcd0c1648d848de815c3
Amlan Chakladar <57117462+amlanc1@users.noreply.github.com>
2023-02-23 15:06:28 -0800

    updated

--- rsbc-ride-producer-api/src/main/java/bcgov/rsbc/ride/kafka/eTkProducer.java
+++ rsbc-ride-producer-api/src/main/java/bcgov/rsbc/ride/kafka/eTkProducer.java
@@ -22,15 +22,39 @@ import io.smallrye.reactive.messaging.kafka.Record;
 
 import bcgov.rsbc.ride.kafka.model.testevent;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 @Path("/etkevents")
 public class eTkProducer {
 
+    private final static Logger logger = LoggerFactory.getLogger(dfProducer.class);
+
+    @Inject
+    @Channel("outgoing-etktestevent")
+    MutinyEmitter<Record<String, String>> emitterTestEvt;
 
 
     @GET
     @Path("/ping")
-    @Produces(MediaType.TEXT_PLAIN)
-    public String hello() {
-        return "etk";
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response etkping() {
+        return Response.ok().entity("{\"status\":\"working\"}").build();
     }
+
+
+    @POST
+    @Produces(MediaType.APPLICATION_JSON)
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Path("/etktestevent")
+    public Response etktestevent(String eventobj) {
+        logger.info(eventobj);
+        Long uid = LocalDateTime.now().toEpochSecond(ZoneOffset.UTC);
+        emitterTestEvt.send(Record.of(uid.toString(), eventobj)).await().atMost(Duration.ofSeconds(5));
+
+        return Response.ok().entity("{\"status\":\"working\"}").build();
+    }
+
+
+
 }
