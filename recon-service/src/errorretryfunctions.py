a9a5a0b7a1988399cf7f15cf8e5323b15e999864
Amlan Chakladar <57117462+amlanc1@users.noreply.github.com>
2023-03-13 11:11:47 -0700

    initial

---
+++ recon-service/src/errorretryfunctions.py
@@ -0,0 +1,49 @@
+import json
+import os
+from producerapifuncs import producerAPITasks
+
+def error_retry_task(dbclient,err_staging_collection,err_table_collection,err_threshold,logger):
+
+    #DONE: Query error staging
+    results = err_staging_collection.find()
+    errretrystatus = True
+    #DONE: For each do below
+    for row in results:
+        # try:
+        if row['event_type'] and row['event_type'] == 'event_1':
+            # DONE: Retry sending to producer api
+            payload_json=json.loads(row['payloadstr'])
+            producer_api_obj=producerAPITasks(os.getenv('PRODUCER_API_HOST'),logger)
+            headers={'api_key':'1234'}
+            success=producer_api_obj.sendAPIReq(row['apipath'],headers,'POST',payload_json)
+            # apiendpoint=f"{os.getenv('PRODUCER_API_HOST')}{row['apipath']}"
+            # print(apiendpoint)
+            # headers = {'Authorization': 'Bearer <access_token>'}
+            # success=True
+            # DONE: if success delete from staging table
+            if success:
+                err_staging_collection.delete_one(row)
+            # DONE: if error update error count
+            else:
+                errretrystatus = False
+                retry_count_val = (lambda x: 1 if not ('retry_count' in x.keys()) else x['retry_count'] + 1)(row)
+                # DONE: if error count more than 10 then delete from staging and move to main error table
+                if retry_count_val>int(err_threshold):
+                    err_staging_collection.delete_one(row)
+                    # DONE: Dedup before saving to error table
+                    query_err_table = err_table_collection.find(row)
+                    if len(list(query_err_table)) > 0:
+                        return True
+                    else:
+                        result = err_table_collection.insert_one(row)
+                        return True
+                else:
+                    new_column = {"$set": {"retry_count": retry_count_val}}
+                    result = err_staging_collection.update_one(row, new_column)
+        # except Exception as e:
+        #     errretrystatus=False
+        #     logger.error('error in retrying for this row')
+        #     logger.error(row)
+        #     logger.error(e)
+
+    return errretrystatus
---
+++ recon-service/src/producerapifuncs.py
@@ -0,0 +1,25 @@
+
+import requests
+
+class producerAPITasks:
+
+    def __init__(self,apihost,logger):
+        self.apihost=apihost
+        self.logger=logger
+
+
+    def sendAPIReq(self,apipath,headers,apimethod,payload):
+        apiurl=f'{self.apihost}{apipath}'
+        apistatus=False
+        try:
+            if apimethod=='POST':
+                response = requests.post(apiurl, headers=headers, data=payload,timeout=10)
+            # print(response.status_code)
+            if response.status_code==200:
+                apistatus=True
+        except Exception as e:
+            self.logger.error('error from the api')
+            self.logger.error(payload)
+            self.logger.error(e)
+
+        return apistatus
