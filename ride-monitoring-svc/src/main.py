0fbce577b55ab1390a7c677e48b58c13d496e120
Amlan Chakladar <57117462+amlanc1@users.noreply.github.com>
2023-04-27 13:50:54 -0700

    aa

--- ride-monitoring-svc/src/main.py
+++ ride-monitoring-svc/src/main.py
@@ -1,10 +1,31 @@
 
 from flask import Flask, jsonify,make_response
+from pymongo import MongoClient
+import os
+
 
 from prometheus_client import Gauge, generate_latest
+import logging
+from metricsfuncs import reconmetrics
+
+numeric_level = getattr(logging, os.getenv('LOG_LEVEL').upper(), 10)
+# Set up logging
+logging.basicConfig(
+    # level=logging[os.getenv('LOG_LEVEL')],
+    level=numeric_level,
+    format='%(asctime)s %(levelname)s %(module)s:%(lineno)d [RIDE_MONITOR]: %(message)s'
+)
 
 app = Flask(__name__)
 
+
+
+client=MongoClient(os.environ.get('MONGO_URL'))
+db = client[os.environ.get('RECON_DB_NAME')]
+main_staging_collection = db[os.getenv('MAIN_STG_COLLECTION')]
+err_staging_collection = db[os.getenv('ERR_STG_COLLECTION')]
+main_table_collection = db[os.getenv('MAIN_TABLE_COLLECTION')]
+err_table_collection=db[os.getenv('ERR_TABLE_COLLECTION')]
 
 
 @app.route('/ping')
