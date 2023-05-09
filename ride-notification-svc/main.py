03b24d11eb0b8fc325c6e9edbc43926e4a31e57b
Amlan Chakladar <57117462+amlanc1@users.noreply.github.com>
2023-05-09 11:27:11 -0700

    aa

--- ride-notification-svc/main.py
+++ ride-notification-svc/main.py
@@ -5,18 +5,11 @@ import fastapi
 from fastapi.responses import PlainTextResponse,JSONResponse
 
 import os
-import logging
 import uvicorn
 
 from src import app
 
-numeric_level = getattr(logging, os.getenv('LOG_LEVEL').upper(), 10)
-# Set up logging
-logging.basicConfig(
-    # level=logging[os.getenv('LOG_LEVEL')],
-    level=numeric_level,
-    format='%(asctime)s %(levelname)s %(module)s:%(lineno)d [RIDE_EMAIL]: %(message)s'
-)
+
 
 api = fastapi.FastAPI()
 
--- ride-notification-svc/src/app.py
+++ ride-notification-svc/src/app.py
@@ -3,9 +3,18 @@ import fastapi
 from fastapi.responses import PlainTextResponse,JSONResponse
 import os
 from src.emailfunctions import EmailFunctions
+import logging
 
 
 router = fastapi.APIRouter()
+
+numeric_level = getattr(logging, os.getenv('LOG_LEVEL').upper(), 10)
+# Set up logging
+logging.basicConfig(
+    # level=logging[os.getenv('LOG_LEVEL')],
+    level=numeric_level,
+    format='%(asctime)s %(levelname)s %(module)s:%(lineno)d [RIDE_EMAIL]: %(message)s'
+)
 
 @router.get('/ping')
 async def pingmethod():
@@ -31,3 +40,11 @@ async def sendbasicemail(payload: dict):
         respstatus = {"status": "failure","error":str(e)}
 
     return JSONResponse(status_code=status_code, content=respstatus)
+
+@router.post('/emailpayloadtest',response_class=JSONResponse)
+async def emailpayloadtest(payload: dict):
+    logging.debug(payload)
+    respstatus = {"status": "success"}
+    status_code = 200
+    return JSONResponse(status_code=status_code, content=respstatus)
+
